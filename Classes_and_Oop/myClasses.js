e8c556aeef0226170ba4cf8ded1f4c9eca80cde0
amangithub9 <iamaman770@gmail.com>
2024-01-28 15:53:34 +0530

    Constructor Both method Done!

--- Classes_and_Oop/myClasses.js
+++ Classes_and_Oop/myClasses.js
@@ -1,23 +1,23 @@
 // ES6
 
-// class User {
-//   constructor(username, email, password) {
-//     this.username = username;
-//     this.email = email;
-//     this.password = password;
-//   }
-//   encryptPassword() {
-//     return `${this.password}abc`;
-//   }
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+  encryptPassword() {
+    return `${this.password}abc`;
+  }
 
-//   changeUsername() {
-//     return `${this.username.toUpperCase()}`;
-//   }
-// }
+  changeUsername() {
+    return `${this.username.toUpperCase()}`;
+  }
+}
 
-// const chai = new User("chai", "chai@gmail.com", "123");
-// console.log(chai.encryptPassword());
-// console.log(chai.changeUsername());
+const chai = new User("chai", "chai@gmail.com", "123");
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
 
 //Behind the scene
 
