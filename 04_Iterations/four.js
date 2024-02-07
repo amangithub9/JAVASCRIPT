f4261e12af4ddd2f35cc8842639dec3d8c054c6e
amangithub9 <iamaman770@gmail.com>
2024-02-07 21:59:58 +0530

    filter

---
+++ 04_Iterations/four.js
@@ -0,0 +1,12 @@
+// const coding = ["CPP", "Java", "Javascript", "Python", "Ruby"];
+
+// const values = coding.forEach((item) =>{
+//     console.log(item);
+//     return item; // forEach do not return any values.
+// });
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+ const number = myNums.filter((num) => num > 6);
+ console.log(number);
