7a26ac99c69df822c2836c6edec6f51aa6b6718c
amangithub9 <iamaman770@gmail.com>
2024-02-12 15:25:43 +0530

    learned a new thing

---
+++ Classes_and_Oop/table.js
@@ -0,0 +1,19 @@
+let countries = [
+    { name: "India", capital: "New Delhi", code: "+91"},
+    { name: "England", capital: "London", code: "+92"},
+    { name: "US", capital: "Washington DC", code: "+94"},
+];
+console.table(countries);
+
+
+let cricketerNames = [
+    {name: "Virat Kohli", century: 80, team: "India"},
+    {name: "MS Dhoni", century: 20, team: "India"},
+    {name: "Steve Smith", century: 50, team: "Australia"},
+    {name: "Jos Buttler", century: 25, team: "England"},
+    {name: "Kane Williomson", century: 46, team: "New Zealand"},
+    {name: "ABD", century: 50, team: "South Africa"},
+
+];
+
+console.table(cricketerNames);
